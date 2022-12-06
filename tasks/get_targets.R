77832d3f2ba1f1a54196bc0fc23fe76d314adca0
nathan kim <nathanckim18@gmail.com>
2022-12-06 01:35:43 -0500

    feature: target uploading functions

--- tasks/get_targets.R
+++ tasks/get_targets.R
@@ -1,4 +1,28 @@
-get_targets <- function(){
+upload_targets <- function(){
+  paste0(
+    "You are about to upload your local _targets directory to `sheriff`, ",
+    "which will overwrite the contents of the directory on that server. "
+  ) %>%
+    str_wrap() %>%
+    message()
+
+  confirmation <- readline("Enter 'y' to proceed; input anything else to quit: ")
+
+  if(confirmation != "y"){
+    stop(paste0("Needed 'y' to proceed, received: ", confirmation))
+  }
+
+  user <- Sys.getenv("SSH_USERNAME")
+  if(user == ""){
+    stop("Must supply a username via the SSH_USERNAME environment variable.
+         Check out README.md for more.")
+  }
+
+  session <- ssh_connect(paste0(user, "@sheriff.ssc.wisc.edu"))
+  scp_upload(session, "_targets", "/var/www/protests_analysis")
+}
+
+download_targets <- function(){
   if(dir.exists("_targets")){
     stop("It looks like you already have a targets directory!
          If you really want to download targets from `sheriff`,
