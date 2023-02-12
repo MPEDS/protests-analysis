a774616d3fd0e7806523aa912956be5db2ea6414
nathan kim <nathanckim18@gmail.com>
2023-02-12 11:52:58 -0500

    updating nixpkgsand fixing whitespace

--- flake.nix
+++ flake.nix
@@ -6,53 +6,53 @@
 
   outputs = { self, nixpkgs, utils } :
     utils.lib.eachDefaultSystem (system:
-		let pkgs = import nixpkgs {
-			inherit system; 
-			overlays = [(self: super: {
-					customRstudio = super.rstudioWrapper.override {
-						packages = [super.rPackages.renv];
-					};
-				})
-			];};
+      let pkgs = import nixpkgs {
+        inherit system; 
+        overlays = [(self: super: {
+          customRstudio = super.rstudioWrapper.override {
+            packages = [super.rPackages.renv];
+          };
+        })
+      ];};
       in {
-        devShell = (pkgs.buildFHSUserEnv {
-					name = "renv-compatible nix";
-          targetPkgs = pkgs:
+        devShells.default = (pkgs.buildFHSUserEnv {
+          name = "renv-compatible nix";
+            targetPkgs = pkgs:
             (with pkgs; [
-							binutils
-							curl.dev
-							gcc
-							libgit2
-							/* Needed for various R dependencies */
-							libssh
-							libpng.dev
+              binutils
+              curl.dev
+              gcc
+              libgit2
+              /* Needed for various R dependencies */
+              libssh
+              libpng.dev
               libtiff.dev
               libjpeg.dev
-							gdal
+              gdal
               geos
               fontconfig.dev
               freetype.dev
               harfbuzz.dev
               fribidi
-							proj.dev
-							libxml2.dev
-							openssl.dev
-							sqlite.dev
-							pandoc
-							zlib.dev
-							unixODBC
-							libmysqlclient.dev
-							udunits
-							pkg-config
-							/* Needed for basic R setup */ 
-							R
-							rPackages.renv
-							customRstudio
-							]);
-							runScript = "bash";
-							profile = ''
-								export R_PROFILE=${builtins.toString ./.}/.Rprofile
-								'';
-					}).env;
-      });
+              proj.dev
+              libxml2.dev
+              openssl.dev
+              sqlite.dev
+              pandoc
+              zlib.dev
+              unixODBC
+              libmysqlclient.dev
+              udunits
+              pkg-config
+              /* Needed for basic R setup */ 
+              R
+              rPackages.renv
+              customRstudio
+              ]);
+              runScript = "bash";
+              profile = ''
+                export R_PROFILE=${builtins.toString ./.}/.Rprofile
+                '';
+          }).env;
+    });
 }
