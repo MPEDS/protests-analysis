53a059668f6a69718e31cdd24f18d98e43b21550
nathan kim <nathanckim18@gmail.com>
2023-07-18 15:15:18 -0400

    revised clustering method for now

--- tasks/mpeds/clean/assign_issue_clusters.R
+++ tasks/mpeds/clean/assign_issue_clusters.R
@@ -4,77 +4,18 @@
 # clusters empirically instead of by hand.
 # This is a VERY inefficient algorithm. I never took an algo class 💀
 
-# weights = c(
-#   column_name = integer
-# )
-compute_distance <- function(event1, event2, points, geo_distances, weights){
-  time1 <- Sys.time()
-  # First get geocoding distance
-  geo_dist <- c(
-    geo_dist = geo_distances[event1$point_id, event2$point_id]
-  )
-
-  # Then compute numerical distances (Manhattan (yuck))
-  # Just kidding we don't have any right now
-
-  time2 <- Sys.time()
-  # Then categorical distances (Dice), which is just the campaign name for us
-  # right now
-  cols <- event1 |> select(where(is.character), where(is.factor)) |> names()
-  categorical_values <- map_dbl(cols, \(col){
-    diff <- isTRUE(event1[[col]] == event2[[col]])
-    diff <- ifelse(event1[[col]], FALSE, diff)
-    return(as.numeric(diff))
-  }) |>
-    set_names(cols)
-
-  time3 <- Sys.time()
-  # Then binary variables, which are pivoted list-cols for this analysis
-  cols <- event1 |> select(where(is.logical)) |> names()
-  binary_values <- map_dbl(cols, \(col){
-    as.numeric(isTRUE(event1[[col]] && event2[[col]]))
-  }) |>
-    set_names(cols)
-
-  time4 <- Sys.time()
-  values <- c(geo_dist, categorical_values, binary_values)
-  score <- sum(values * weights[names(values)], na.rm = TRUE)
-
-  time5 <- Sys.time()
-  print(c(time1 = time2-time1, time2 = time3 - time2, time3 = time4 - time3, time4 = time5 - time4))
-  return(score)
-}
-
 assign_clusters <- function(geocoded, canonical_event_relationship){
-  # Precompute spatial distances, since we have some functions for doing that
-  # up front
   geocoded <- geocoded |>
-    st_as_sf(coords = c("location_lng", "location_lat"), na.fail = FALSE)
-  points <- geocoded |>
-    select() |>
-    distinct() |>
-    pull(geometry)
-  geo_distances <- st_distance(points, points)
-  # closer to 1 = closer to each other geographically
-  geo_distances <- 1 - geo_distances/max(geo_distances, na.rm = TRUE)
-
-  # Join campaign column
-  campaigns <- canonical_event_relationship |>
-    filter(relationship_type == "campaign") |>
-    group_by(canonical_id1) |>
-    slice_head(n = 1)
-  geocoded <- geocoded |>
-    left_join(campaigns, by = c("canonical_id" = "canonical_id1")) |>
-    rename(campaign_id = canonical_id2) |>
-    mutate(campaign_id = ifelse(is.na(campaign_id), 0, campaign_id),
-           campaign_id = as.character(campaign_id))
+    st_as_sf(coords = c("location_lng", "location_lat"), na.fail = FALSE) |>
+    filter(!st_is_empty(geometry))
 
   # Select only columns that will be used for clustering
   # Just using issue and racial_issue for now for proof of concept/prototype run
-  geometry <- geocoded |> select(key)
-  geocoded <- geocoded[1:100,] |>
+  geometries <- geocoded|>
+    select(key)
+  cluster_inputs <- geocoded|>
     st_drop_geometry() |>
-    select(key, campaign_id, issue, racial_issue) |>
+    select(key, issue, racial_issue) |>
     unnest(issue) |>
     distinct() |>
     mutate(dummy = TRUE, issue = paste0("issue__", issue)) |>
@@ -84,37 +25,58 @@ assign_clusters <- function(geocoded, canonical_event_relationship){
     mutate(dummy = TRUE, racial_issue = paste0("racial_issue__", racial_issue)) |>
     pivot_wider(names_from = racial_issue, values_from = dummy,
                 values_fill = FALSE) |>
-    left_join(geometry, by = "key") |>
-    mutate(point_id = lmap(geometry, ~list(which(points == .))) |>
-             unlist()) |>
+    left_join(geometries, by = "key") |>
     select(-contains("Not relevant"), -key)
 
+  # Geographic distance matrix
+  geo_distances <- st_distance(cluster_inputs$geometry, cluster_inputs$geometry)
+  # closer to 1 = closer to each other geographically
+  geo_distances <- 1 - geo_distances/max(geo_distances, na.rm = TRUE)
+  cluster_inputs <- select(cluster_inputs, -geometry)
+
   base_weights <- c(
-    campaign_id = 1,
     issue = 1,
     racial_issue = 1
   )
   weights <- base_weights |>
     lmap(\(weight){
-      matches <- str_subset(names(geocoded), paste0("^", names(weight)))
+      matches <- str_subset(names(cluster_inputs), paste0("^", names(weight)))
       multiplier <- 1 / length(matches)
       list(rep(multiplier, length(matches)) |> set_names(matches))
-    })
-  weights <- c(geo_dist = 1, weights) |> unlist()
+    }) |>
+    unlist()
+  weights <- weights[names(cluster_inputs)]
 
-  iterable_grid <- expand_grid(event1 = 1:nrow(geocoded), event2 = 1:nrow(geocoded)) |>
-    filter(event1 < event2)
+  # distance (by gower) for the rest of the columns
+  distance_matrix <- cluster_inputs |>
+    daisy(metric = "gower",
+          weights = weights) |>
+    as.matrix()
+
+  # add in the geographic distances
+  distance_matrix <- sum(weights)/(1+sum(weights)) + distance_matrix + 1/sum(weights) * geo_distances
 
   start <- Sys.time()
-  distances <- iterable_grid[1:100,] |>
-    mutate(distances = map2_dbl(event1, event2, \(id1, id2){
-      compute_distance(geocoded[id1,], geocoded[id2,], points, geo_distances, weights)
-    }, .progress = TRUE))
+  clusters <- pam(distance_matrix, k = 200, diss = TRUE)
   message(Sys.time() - start)
+
+  sil_width <- NA
+  for(i in 2:10){
+    pam_fit <- pam(distance_matrix,
+                   diss = TRUE,
+                   k = i)
+    sil_width[i] <- pam_fit$silinfo$avg.width
+
+  }
+
+  # Plot sihouette width (higher is better)
+  plot(1:10, sil_width,
+       xlab = "Number of clusters",
+       ylab = "Silhouette Width")
+  lines(1:10, sil_width)
 
   # pivot list-cols into one column each
   # create weights list so that each original column has about equal weight
-  # save for campaign, which is weighted way more heavily
 
   cluster_assignments <- cluster_assignments |>
     group_by(key) |>
