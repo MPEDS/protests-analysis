19b829386d5e8065bae691e5136c1523059a099d
falseiftrue <73044386+falseiftrue@users.noreply.github.com>
2024-09-05 08:46:34 -0400

    updated umbrella analyses get_big_umbrella()

--- tasks/one-off/get_labor_movement_orgs.R
+++ tasks/one-off/get_labor_movement_orgs.R
@@ -1,16 +1,21 @@
 
 
 
-get_unions <- function() {
+get_labor_movement_orgs <- function() {
 
   integrated <- tar_read(integrated)
 
   labor_protests <- integrated |>
     st_drop_geometry() |>
-    select(canonical_id, issue, movement_organizations_text) |>
-    filter(issue == "Labor and work")
-    # unnest(movement_organizations_text)
-    # unnest(c(issue, movement_organizations_text)) |>
-    # mutate(y = fun(x, y, z)) |> unnest(y)
+    filter(map_lgl(issue, ~"Labor and work" %in% .)) |>
+    select(canonical_id, key, issue, movement_organizations_text)
 
+  labor_movement_orgs <- labor_protests |>
+    filter(!is.na(movement_organizations_text)) |>
+    unnest(movement_organizations_text)
+
+  writexl::write_xlsx(labor_movement_orgs,
+                      "docs/data-cleaning-requests/labor_movement_orgs.xlsx")
+
+  return(labor_movement_orgs)
 }
