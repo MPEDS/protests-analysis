8d045b9a0e55c18da0f7c7b3197b28c01e178454
falseiftrue <73044386+falseiftrue@users.noreply.github.com>
2024-09-09 21:05:47 -0400

    added get_police_activities_monitor()

---
+++ tasks/one-off/get_police_activities_monitor.R
@@ -0,0 +1,22 @@
+# Please generate a spreadsheet for any protests that are coded with
+# Police Actions: Monitor/Present and also have any Police Action field
+# coded other than these three: Cooperate, Participate, Breaking the Rules
+
+get_police_activities_monitor <- function() {
+
+  tar_load(integrated)
+
+  police_responses <- integrated |>
+    st_drop_geometry() |>
+    filter(map_lgl(police_activities, ~"Monitor/Present" %in% .),
+           map_lgl(police_activities, \(activities){
+             length(activities[!(activities %in% c("Cooperate/Coordinate", '"Breaking the Rules"',
+                                                   "Participate"))]) > 1
+           })) |>
+    select(canonical_id, key, description, police_activities)
+
+  writexl::write_xlsx(police_responses,
+                      "docs/data-cleaning-requests/police_responses_monitor.xlsx")
+
+}
+
