b4689c76546d29642fb05f86c65b1cc44435eb56
falseiftrue <73044386+falseiftrue@users.noreply.github.com>
2024-10-07 14:00:59 -0400

    changes to get_controversial_speakers()

--- tasks/one-off/get_controversial_speakers.R
+++ tasks/one-off/get_controversial_speakers.R
@@ -8,9 +8,14 @@ get_controversial_speakers <- function() {
   integrated <- integrated |>
     st_drop_geometry()
 
-  controverisal <- integrated |>
-    filter(map_lgl(target_text, ~any(str_detect(., "Planned Parenthood")))) |>
-    select(canonical_id, key, issue, issues_text, target, target_text) |>
+  controversial_speakers <- integrated |>
+    filter(
+      map_lgl(description, ~any(str_detect(., "Milo Yiannopoulos|Ann Coulter|Richard Spencer|Charles Murray|Gavin McInnes|Ezra Levant|Heather MacDonald|Jordan Peterson"))) |
+      map_lgl(issues_text, ~any(str_detect(., "Milo Yiannopoulos|Ann Coulter|Richard Spencer|Charles Murray|Gavin McInnes|Ezra Levant|Heather MacDonald|Jordan Peterson")))) |>
+    select(canonical_id, key, description, issue, issues_text) |>
     mutate(across(where(is.list), ~map_chr(., ~paste(., collapse = ", "))))
 
+
+  writexl::write_xlsx(controversial_speakers,
+                      "docs/data-cleaning-requests/low-level-data-cleaning/controversial_speakers.xlsx")
 }
